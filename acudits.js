1ebe9c974a85ddb42085dc2b9a357f20b8684742
Marc <markus.mp11@gmail.com>
2024-06-12 23:45:24 +0200

    S.4 Api nivell2

--- acudits.js
+++ acudits.js
@@ -1,7 +1,10 @@
-var button = document.querySelector("button");
+var button = document.querySelector(".btn");
 var cardText = document.querySelector(".card-text");
+var btn1 = document.querySelector(".btn1");
+var btn2 = document.querySelector(".btn2");
+var btn3 = document.querySelector(".btn3");
 var reportJokes = [];
-// Función para traer una broma de la API
+// Función para traer datos de la API
 function traerJoke() {
     var options = {
         method: 'GET',
@@ -12,23 +15,24 @@ function traerJoke() {
     fetch("https://icanhazdadjoke.com/", options)
         .then(function (res) { return res.json(); })
         .then(function (data) {
+        console.log(data);
         mostrarJoke(data);
-        capturarJoke(data);
-        capturarIsoDate();
+        capturarDatosJoke(data);
+        console.log(reportJokes);
     })
         .catch(function (error) {
         console.error("Error fetching joke:", error);
     });
 }
-// Función para crear y añadir una broma al contenedor
+// Función para crear y mostrar broma (Api) en la cart
 function mostrarJoke(data) {
-    //validacion cardText por si es null o undefined
+    //validacion cardText por si no existe
     if (!cardText) {
         console.error("card text no se encuentra.");
         return;
     }
     var p = cardText.querySelector("p");
-    // Si el elemento <p> no existe, lo creamos 
+    // Si el elemento <p> no existe, lo creamos y si existe que es el caso, mostramos la broma en este
     if (!p) {
         p = document.createElement("p");
         cardText.appendChild(p);
@@ -42,18 +46,21 @@ function mostrarJoke(data) {
 traerJoke();
 //botón para traer una nueva broma al hacer clic
 button === null || button === void 0 ? void 0 : button.addEventListener("click", traerJoke);
-// botones para function asignar score
-// document.getElementById("btn").addEventListener("click", traerJoke): 
-// document.getElementById("btn1").addEventListener("click", capturarDatos)
-// document.getElementById("btn2").addEventListener("click", capturarDatos)
-// document.getElementById("btn3").addEventListener("click", capturarDatos)
-function capturarJoke(data) {
-    reportJokes.push(data.joke);
-    console.log(reportJokes);
+//botones para capturar score segun boton clickado
+btn1 === null || btn1 === void 0 ? void 0 : btn1.addEventListener("click", function () { capturarScore(1); });
+btn2 === null || btn2 === void 0 ? void 0 : btn2.addEventListener("click", function () { capturarScore(2); });
+btn3 === null || btn3 === void 0 ? void 0 : btn3.addEventListener("click", function () { capturarScore(3); });
+// funcion para capturar y agregar al array los datos de la broma actual. ScoreActual por defecto es null ya que es opcional que el usuario lo indique
+function capturarDatosJoke(data) {
+    var jokeActual = data.joke;
+    var isoDateActual = new Date().toISOString();
+    var scoreActual = null;
+    var nuevoChiste = { joke: jokeActual, score: scoreActual, date: isoDateActual };
+    reportJokes.push(nuevoChiste);
 }
-function capturarIsoDate() {
-    var isoDate = new Date().toISOString();
-    console.log(isoDate);
-    reportJokes.push(isoDate);
-    console.log(reportJokes);
+// funcion que actualiza la propiedad score de la broma actual si el usuario valora la broma clicando emoticonos
+function capturarScore(num) {
+    var scoreActual = num;
+    console.log(num);
+    reportJokes[reportJokes.length - 1].score = scoreActual;
 }
--- acudits.ts
+++ acudits.ts
@@ -1,14 +1,25 @@
 
-const button: HTMLButtonElement | null = document.querySelector("button")
+const button: HTMLButtonElement | null = document.querySelector(".btn")
 const cardText: HTMLParagraphElement | null = document.querySelector(".card-text")
+const btn1: HTMLButtonElement | null = document.querySelector(".btn1");
+const btn2: HTMLButtonElement | null = document.querySelector(".btn2");
+const btn3: HTMLButtonElement | null = document.querySelector(".btn3");
 
-const reportJokes: Object[] = []
 
 interface JokeData {
     joke: string;
 }
 
-// Función para traer una broma de la API
+interface chiste {
+    joke: string;
+    score: number | null;
+    date: string;
+}
+
+const reportJokes: chiste[] = []
+
+
+// Función para traer datos de la API
 function traerJoke() : void {
     const options = {
         method: 'GET',
@@ -20,9 +31,11 @@ function traerJoke() : void {
     fetch("https://icanhazdadjoke.com/", options)
         .then((res) => res.json())
         .then(data => {
+            console.log(data)
             mostrarJoke(data)
-            capturarJoke(data)
-            capturarIsoDate()
+            capturarDatosJoke(data)
+            console.log(reportJokes)
+            
 
         }
     ) 
@@ -32,9 +45,9 @@ function traerJoke() : void {
 }
 
 
-// Función para crear y añadir una broma al contenedor
+// Función para crear y mostrar broma (Api) en la cart
 function mostrarJoke(data: JokeData): void {
-    //validacion cardText por si es null o undefined
+    //validacion cardText por si no existe
     if (!cardText) {
         console.error("card text no se encuentra.");
         return;
@@ -42,14 +55,13 @@ function mostrarJoke(data: JokeData): void {
 
     let p: HTMLParagraphElement | null = cardText.querySelector("p");
 
-    // Si el elemento <p> no existe, lo creamos 
+    // Si el elemento <p> no existe, lo creamos y si existe que es el caso, mostramos la broma en este
     if (!p) {
         p = document.createElement("p");
         cardText.appendChild(p);
         p.textContent = data.joke;
         return;
-
-    
+        
     }else {
         p.textContent = data.joke;
     }
@@ -61,23 +73,27 @@ traerJoke()
 //botón para traer una nueva broma al hacer clic
 button?.addEventListener("click", traerJoke);
 
-// botones para function asignar score
-// document.getElementById("btn").addEventListener("click", traerJoke): 
-// document.getElementById("btn1").addEventListener("click", capturarDatos)
-// document.getElementById("btn2").addEventListener("click", capturarDatos)
-// document.getElementById("btn3").addEventListener("click", capturarDatos)
+//botones para capturar score segun boton clickado
+btn1?.addEventListener("click", function(){capturarScore(1)});
+btn2?.addEventListener("click", function(){capturarScore(2)});
+btn3?.addEventListener("click", function(){capturarScore(3)});
 
+// funcion para capturar y agregar al array los datos de la broma actual. ScoreActual por defecto es null ya que es opcional que el usuario lo indique
+function capturarDatosJoke(data: JokeData): void {
+    let jokeActual = data.joke
+    let isoDateActual = new Date().toISOString();
+    let scoreActual = null
 
+    let nuevoChiste: chiste = { joke: jokeActual, score: scoreActual, date: isoDateActual };
+    reportJokes.push(nuevoChiste)
 
-function capturarJoke(data: JokeData): void {
-    reportJokes.push(data.joke)
-    console.log(reportJokes)
 }
 
-function capturarIsoDate(){
-    const isoDate = new Date().toISOString();
-    console.log(isoDate)
-    reportJokes.push(isoDate)
-    console.log(reportJokes)
-}
+// funcion que actualiza la propiedad score de la broma actual si el usuario valora la broma clicando emoticonos
+function capturarScore(num: number): void {
+    let scoreActual = num
+    console.log(num)
+    reportJokes[reportJokes.length-1].score = scoreActual
+
+} 
 
